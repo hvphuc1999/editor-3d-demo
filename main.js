594a0544816dfe9151c58d31e46d287999d0875b
PhucHoang <hvphuc081299@gmail.com>
2025-04-18 20:16:38 +0700

    implement transform property

--- main.js
+++ main.js
@@ -47,7 +47,9 @@ function main() {
       camera,
       renderer.domElement
     );
-    transformControl.addEventListener("change", render);
+    transformControl.addEventListener("change", () => {
+      handleTransformObjectChange();
+    });
     transformControl.addEventListener("dragging-changed", function (event) {
       orbitControl.enabled = !event.value;
     });
@@ -148,6 +150,18 @@ function main() {
     objectTitleEle.textContent = `${type}-${meshId}`;
     objectEle.appendChild(objectTitleEle);
 
+    const objectTrashEle = document.createElement("img");
+    objectTrashEle.id = `trash-${mesh.uuid}`
+    objectTrashEle.src = "./trash.svg";
+    objectTrashEle.width = 18;
+    objectTrashEle.height = 18;
+    objectTrashEle.style = "visibility: hidden";
+    objectEle.appendChild(objectTrashEle);
+    objectTrashEle.addEventListener("click", (e) => {
+      e.stopPropagation();
+      handleRemoveMesh(mesh);
+    });
+
     objectListEle.appendChild(objectEle);
     objectList.push({
       id: meshId,
@@ -167,7 +181,7 @@ function main() {
     if (!currentSelectedObjectEle) return;
     currentSelectedObjectEle.classList.remove("object-item--selected");
     const trash = document.getElementById(`trash-${currentSelectedObjectEle.id}`);
-    if (trash) trash.remove();
+    if (trash) trash.style = 'visibility: hidden';
     const currentSelectedObject = objectList.find(item => item.id === currentSelectedObjectEle.id);
     if (currentSelectedObject) {
       const gizmo = transformControl.getHelper();
@@ -228,16 +242,8 @@ function main() {
     scene.add(gizmo);
     /* ===================== */
 
-    const objectTrashEle = document.createElement("img");
-    objectTrashEle.id = `trash-${mesh.uuid}`
-    objectTrashEle.src = "./trash.svg";
-    objectTrashEle.width = 18;
-    objectTrashEle.height = 18;
-    objectEle.appendChild(objectTrashEle);
-    objectTrashEle.addEventListener("click", (e) => {
-      e.stopPropagation();
-      handleRemoveMesh(mesh);
-    });
+    const objectTrashEle = document.getElementById(`trash-${mesh.uuid}`);
+    if (objectTrashEle) objectTrashEle.style = "display: block"
 
     render();
 
@@ -248,55 +254,161 @@ function main() {
     const objectPropertiesEle = document.querySelector('.object-properties')
 
     /* Transform property */
-    const transformPropertyEle = document.createElement('div');
-    transformPropertyEle.classList.add('object-property');
-    const moveEle = document.createElement('div');
-    moveEle.classList.add('transform-icon');
-    moveEle.classList.add('transform-icon--selected');
-    const moveIcon = document.createElement('img');
-    moveIcon.src = './move.svg';
-    moveEle.appendChild(moveIcon);
-    moveEle.addEventListener('click', () => {
-      const currentSelectedModeEle = document.querySelector('.transform-icon--selected')
-      currentSelectedModeEle.classList.remove('transform-icon--selected')
-      moveEle.classList.add('transform-icon--selected');
-      transformControl.setMode('translate');
-    });
-    transformPropertyEle.appendChild(moveEle)
+    const transformIconPropertyEle = document.createElement('div');
+    transformIconPropertyEle.classList.add('object-property');
+    transformIconPropertyEle.style = "display: flex; align-items: center; column-gap: 2rem;"
 
-    const rotateEle = document.createElement('div');
-    rotateEle.classList.add('transform-icon');
-    const rotateIcon = document.createElement('img');
-    rotateIcon.src = './rotate.svg';
-    rotateEle.appendChild(rotateIcon);
-    rotateEle.addEventListener('click', () => {
-      const currentSelectedModeEle = document.querySelector('.transform-icon--selected')
-      currentSelectedModeEle.classList.remove('transform-icon--selected')
-      rotateEle.classList.add('transform-icon--selected');
-      transformControl.setMode('rotate');
-    });
-    transformPropertyEle.appendChild(rotateEle)
+    const transformIconList = [
+      {
+        selectedDefault: true,
+        iconPath: './move.svg',
+        mode: 'translate',
+      },
+      {
+        iconPath: './rotate.svg',
+        mode: 'rotate',
+      },
+      {
+        iconPath: './scale.svg',
+        mode: 'scale',
+      },
+    ]
+    const renderTransformIcon = ({ selectedDefault = false, iconPath, mode }) => {
+      const transformTypeEle = document.createElement('div');
+      transformTypeEle.classList.add('transform-icon');
+      if (selectedDefault) transformTypeEle.classList.add('transform-icon--selected');
+      const iconEle = document.createElement('img');
+      iconEle.src = iconPath;
+      transformTypeEle.appendChild(iconEle);
+      transformTypeEle.addEventListener('click', () => {
+        const currentSelectedModeEle = document.querySelector('.transform-icon--selected')
+        currentSelectedModeEle.classList.remove('transform-icon--selected')
+        transformTypeEle.classList.add('transform-icon--selected');
+        transformControl.setMode(mode);
+      });
+      transformIconPropertyEle.appendChild(transformTypeEle)
+    }
+    transformIconList.forEach(renderTransformIcon)
 
-    const scaleEle = document.createElement('div');
-    scaleEle.classList.add('transform-icon');
-    const scaleIcon = document.createElement('img');
-    scaleIcon.src = './scale.svg';
-    scaleEle.appendChild(scaleIcon);
-    scaleEle.addEventListener('click', () => {
-      const currentSelectedModeEle = document.querySelector('.transform-icon--selected')
-      currentSelectedModeEle.classList.remove('transform-icon--selected')
-      scaleEle.classList.add('transform-icon--selected');
-      transformControl.setMode('scale');
-    });
-    transformPropertyEle.appendChild(scaleEle)
+    objectPropertiesEle.appendChild(transformIconPropertyEle);
+    /* ===================== */
 
-    objectPropertiesEle.appendChild(transformPropertyEle);
+    /* Position, Rotation, Scale property */
+    const transformTypeByAxisList = ['position', 'rotation', 'scale']
+    const renderTransformByAxisProperty = (type) => {
+      const axisPropertyEle = document.createElement('div');
+      axisPropertyEle.classList.add('object-property');
+  
+      const titleEle = document.createElement('div')
+      titleEle.textContent = type.slice(0, 1).toUpperCase() + type.slice(1)
+      titleEle.style = 'font-size: 1.25rem; margin-bottom: 1rem;'
+      axisPropertyEle.appendChild(titleEle);
+  
+      const xyzContainerEle = document.createElement('div');
+      xyzContainerEle.classList.add('xyz-axis-input-container');
+
+      const axisInputList = [
+        {
+          axisName: 'X',
+          inputId:  `${type}-x-input`,
+        },
+        {
+          axisName: 'Y',
+          inputId: `${type}-y-input`,
+        },
+        {
+          axisName: 'Z',
+          inputId: `${type}-z-input`,
+        },
+      ]
+      const renderAxisInput = ({ axisName, inputId }) => {
+        const axisContainerEle = document.createElement('div');
+        const axisEle = document.createElement('div');
+        axisEle.textContent = axisName
+        axisEle.style = "margin-bottom: 0.5rem"
+        const axisInputEle = document.createElement('input');
+        axisInputEle.id = inputId;
+        axisInputEle.type = 'number';
+        axisInputEle.value = mesh?.[`${type}`].x;
+        axisInputEle.onchange = (e) => {
+          const value = e.target.value;
+          if (axisName.toLowerCase() === 'x') mesh.scale.set(value, mesh?.[`${type}`].y, mesh?.[`${type}`].z)
+          if (axisName.toLowerCase() === 'y') mesh.scale.set(mesh?.[`${type}`].x, value, mesh?.[`${type}`].z)
+          if (axisName.toLowerCase() === 'z') mesh.scale.set(mesh?.[`${type}`].x, mesh?.[`${type}`].y, value)
+          render()
+        }
+        axisContainerEle.appendChild(axisEle);
+        axisContainerEle.appendChild(axisInputEle);
+        xyzContainerEle.appendChild(axisContainerEle);
+      }
+      axisInputList.forEach(renderAxisInput);
+  
+      axisPropertyEle.appendChild(xyzContainerEle);
+      objectPropertiesEle.appendChild(axisPropertyEle);
+    }
+    transformTypeByAxisList.forEach(renderTransformByAxisProperty);
     /* ===================== */
   }
 
   function removePropertiesSection() {
     const objectPropertiesEle = document.querySelector('.object-properties')
     objectPropertiesEle.innerHTML = '';
+  }
+
+  function handleTransformObjectChange() {
+    const listChangeId = [
+      {
+        inputId: 'position-x-input',
+        type: 'position',
+        axisName: 'x',
+      },
+      {
+        inputId: 'position-y-input',
+        type: 'position',
+        axisName: 'y',
+      },
+      {
+        inputId: 'position-z-input',
+        type: 'position',
+        axisName: 'z',
+      },
+      {
+        inputId: 'rotation-x-input',
+        type: 'rotation',
+        axisName: 'x',
+      },
+      {
+        inputId: 'rotation-y-input',
+        type: 'rotation',
+        axisName: 'y',
+      },
+      {
+        inputId: 'rotation-z-input',
+        type: 'rotation',
+        axisName: 'z',
+      },
+      {
+        inputId: 'scale-x-input',
+        type: 'scale',
+        axisName: 'x',
+      },
+      {
+        inputId: 'scale-y-input',
+        type: 'scale',
+        axisName: 'y',
+      },
+      {
+        inputId: 'scale-z-input',
+        type: 'scale',
+        axisName: 'z',
+      },
+    ]
+    const handleChangeAxis = ({ inputId, type, axisName }) => {
+      const inputEle = document.getElementById(inputId);
+      if (inputEle && transformControl) inputEle.value = transformControl?.object?.[`${type}`]?.[`${axisName}`];
+    }
+    listChangeId.forEach(handleChangeAxis);
+    render()
   }
 }
 
